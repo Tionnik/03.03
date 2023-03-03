2e53415796848a590e196b1d979f7c51c4c77644
Nik <ice_fire50@mail.ru>
2023-03-03 17:13:55 +0300

    task 52

---
+++ task 50/Program.cs
@@ -0,0 +1,30 @@
+﻿int arrey(int m,int n) {
+    int [,] mass = new int [10,10];
+    for (int i =0; i<10; i++){
+        for (int j=0; j<10;j++){
+            mass[i,j]=new Random().Next(-10,11);
+        }
+    }
+int A = mass[m-1,n-1];
+//print(mass);
+return A;
+}
+/*
+void print (int [,] mass){
+    for (int i =0; i<mass.GetLength(0); i++){
+        for (int j=0; j<mass.GetLength(1); j++){
+            Console.Write(mass[i,j]+" ");
+        }
+    Console.WriteLine();
+    }
+}
+*/
+Console.Clear();									
+Console.WriteLine("Введите номер строки");
+int m = int.Parse(Console.ReadLine());						
+Console.WriteLine("Введите номер элемента");
+int n = int.Parse(Console.ReadLine());
+if (m>10 || n>10){                                                          //Если позиция != индекс
+    Console.WriteLine("такого числа в массиве нет");}
+    else Console.WriteLine(arrey(m,n));
+
---
+++ task 52/Program.cs
@@ -0,0 +1,36 @@
+﻿int [,] arrey(int m,int n) {
+    int [,] mass = new int [m,n];
+    for (int i =0; i<m; i++){
+        for (int j=0; j<n;j++){
+            mass[i,j]=new Random().Next(-10,11);
+        }
+    }
+//print(mass);
+return mass;
+}
+void math(int [,] mass){
+//Console.WriteLine();
+    for (int j =0; j<mass.GetLength(0); j++){
+        int sum =0;
+        for (int i=0; i<mass.GetLength(1);i++){
+            sum= sum+mass[i,j];
+        }
+    Console.Write(Math.Round((double) sum/mass.GetLength(0), 1)+"; ");
+    }
+}
+/*
+void print (int [,] mass){
+    for (int i =0; i<mass.GetLength(0); i++){
+        for (int j=0; j<mass.GetLength(1); j++){
+            Console.Write(mass[i,j]+" ");
+        }
+    Console.WriteLine();
+    }
+}*/
+
+Console.Clear();									
+Console.WriteLine("Введите колличество строк");
+int m = int.Parse(Console.ReadLine());						
+Console.WriteLine("Введите колличество элементов");
+int n = int.Parse(Console.ReadLine());
+math(arrey(m,n));
